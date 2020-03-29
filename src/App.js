01afbcf574aa52217070c754242f4f926d4a2812
Michel-Alexandre Belanger <michel-alexandre.belanger@tritondigital.com>
2020-03-29 19:28:01 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 function App() {
-  const videoRef = useRef(null);
+  let localVideoRef = useRef(null);
+  let remoteVideoRef = useRef(null);
+  let textRef = useRef(null);
+
+  const pcConfig = null;
+  // const pcConfig = {
+  //   "iceServers": [{
+  //     urls: '',
+  //     credential: '',
+  //     username: ''
+  //   }]
+  // }
+  const pc = new RTCPeerConnection(pcConfig);
 
   function onSuccess(stream) {
-    videoRef.current.srcObject = stream;
+    const tracks = stream.getTracks();
+    tracks.forEach(track =>
+      pc.addTrack(track, (localVideoRef.current.srcObject = stream))
+    );
+    // pc.addTrack(tracks[0]);
   }
 
   function onFailure(error) {
@@ -19,13 +35,90 @@ function App() {
     navigator.getUserMedia({ video: true }, onSuccess, onFailure);
   }
 
+  function connectPeer() {
+    pc.onicecandidate = e => {
+      if (e.candidate) {
+        console.log(JSON.stringify(e.candidate));
+      }
+    };
+
+    pc.oniceconnectionstatechange = e => {
+      console.log(e);
+    };
+
+    pc.ontrack = e => {
+      console.log("e", e);
+      remoteVideoRef.current.srcObject = e.streams[0];
+    };
+  }
+
+  function createOffer() {
+    console.log("Offer");
+    pc.createOffer({ offerToReceiveVideo: 1 })
+      .then(sdp => {
+        console.log(JSON.stringify(sdp));
+        pc.setLocalDescription(sdp);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  function setRemoteDescription() {
+    const desc = JSON.parse(textRef.current.value);
+    pc.setRemoteDescription(new RTCSessionDescription(desc));
+  }
+
+  function createAnswer() {
+    console.log("Answer");
+    pc.createAnswer({ offerToReceiveVideo: 1 })
+      .then(sdp => {
+        console.log(JSON.stringify(sdp));
+        pc.setLocalDescription(sdp);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  function addCandidate() {
+    const candidate = JSON.parse(textRef.current.value);
+    console.log("Adding candidate:", candidate);
+    pc.addIceCandidate(new RTCIceCandidate(candidate));
+  }
+
   useEffect(() => {
+    connectPeer();
     startCamera();
   }, []);
 
+  function renderVideo(ref, color) {
+    return (
+      <div>
+        <video
+          autoPlay
+          ref={ref}
+          style={{
+            width: 240,
+            height: 240,
+            margin: 5,
+            backgroundColor: color
+          }}
+        ></video>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <video ref={videoRef} autoPlay></video>
+      {renderVideo(localVideoRef, "black")}
+      <button onClick={createOffer}>Offer</button>
+      <button onClick={createAnswer}>Answer</button>
+      <br />
+      <textarea ref={textRef} />
+      <button onClick={setRemoteDescription}>setRemoteDescription</button>
+      <button onClick={addCandidate}>addCandidate</button>
+      {renderVideo(remoteVideoRef, "yellow")}
     </div>
   );
 }
